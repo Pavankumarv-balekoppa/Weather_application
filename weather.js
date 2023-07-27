1ef58425cc9730322211c17a239d536784752139
Pavankumarv-balekoppa <pavankumarv221@gmail.com>
2023-07-27 13:00:26 +0530

    pushing the weather application

---
+++ weather.js
@@ -0,0 +1,24 @@
+//http://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric&appid=1cb6532aea3c298a830a71380eace21e
+
+let city=document.getElementById('city')
+let country=document.getElementById('country')
+let digri=document.getElementById('digri')
+let user_input=document.getElementById('user_input')
+
+let searchTemp=async()=>
+{
+    let cityvalue=user_input.value
+   if(cityvalue=="")
+   {
+    alert('please enter city name before search')
+   }
+   else
+   {
+    // alert(`You entered city is :  ${cityvalue}`)
+    let response=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityvalue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`)
+    let data=await response.json()
+    city.innerText=data.name
+    country.innerText=data.sys.country
+    digri.innerText=data.main.temp
+   }
+}
