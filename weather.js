6a7f5945caddc0c1ba5a023db089bfbf1555da5d
Pavankumarv-balekoppa <pavankumarv221@gmail.com>
2023-08-04 10:33:13 +0530

    make a url secure

--- weather.js
+++ weather.js
@@ -15,7 +15,7 @@ let searchTemp=async()=>
    else
    {
     // alert(`You entered city is :  ${cityvalue}`)
-    let response=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityvalue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`)
+    let response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityvalue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`)
     let data=await response.json()
     city.innerText=data.name
     country.innerText=data.sys.country
